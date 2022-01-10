844bb7a05d77d7615b40d038e6e05686038e6e9a
William Noris <w.noris@protonmail.com>
2022-01-10 13:01:06 +0100

    filtre prix fonctionnel

--- src/app/components/filter-side-bar/filter-side-bar.component.ts
+++ src/app/components/filter-side-bar/filter-side-bar.component.ts
@@ -1,18 +1,39 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-filter-side-bar',
   templateUrl: './filter-side-bar.component.html',
-  styleUrls: ['./filter-side-bar.component.scss']
+  styleUrls: ['./filter-side-bar.component.scss'],
 })
 export class FilterSideBarComponent implements OnInit {
   @Input() listCategories: string[];
-  
-  constructor() { 
+  @Output() filterPrice = new EventEmitter();
+
+  constructor() {
     this.listCategories = [];
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onFilterPriceClick() {
+    const minValue = (<HTMLInputElement>(
+      document.getElementById('filter-price-min')
+    )).value;
+    const maxValue = (<HTMLInputElement>(
+      document.getElementById('filter-price-max')
+    )).value;
+    const minVal = minValue ? parseInt(minValue) : 0;
+    const maxVal = maxValue ? parseInt(maxValue) : 0;
+    this.filterPrice.emit(new MinMax(minVal, maxVal));
+  }
+}
+
+export class MinMax {
+  min: number;
+  max: number;
+
+  constructor(private minVal: number = 0, private maxVal: number = 0) {
+    this.min = minVal;
+    this.max = maxVal;
+  }
 }
--- src/app/pages/page-accueil/page-accueil.component.ts
+++ src/app/pages/page-accueil/page-accueil.component.ts
@@ -1,65 +1,81 @@
 import { Component, OnInit } from '@angular/core';
+import { MinMax } from 'src/app/components/filter-side-bar/filter-side-bar.component';
 import { PlantouneService } from 'src/app/services/plantoune.service';
 import * as _ from 'underscore';
 
 @Component({
   selector: 'app-page-accueil',
   templateUrl: './page-accueil.component.html',
-  styleUrls: ['./page-accueil.component.scss']
+  styleUrls: ['./page-accueil.component.scss'],
 })
 export class PageAccueilComponent implements OnInit {
   public listData: any[];
   public listCategoriesFilter: string[];
+  public listDataFilteredByPrice: any[];
 
   constructor(private plantouneService: PlantouneService) {
     this.listData = [];
     this.listCategoriesFilter = [];
-   }
+    this.listDataFilteredByPrice = [];
+  }
 
-   /**
-    * equivalent de la ligne du dessus 
-    * 
-    * plantouneService;
-    * 
-    * constructor(plantouneService: PlantouneService) {
-    *   this.plantouneService = plantouneService;
-    * }
-    */
-
-
+  /**
+   * equivalent de la ligne du dessus
+   *
+   * plantouneService;
+   *
+   * constructor(plantouneService: PlantouneService) {
+   *   this.plantouneService = plantouneService;
+   * }
+   */
 
   ngOnInit(): void {
+    this.plantouneService.getData().subscribe((listPlant: any[]) => {
+      console.log(listPlant);
 
-    this.plantouneService.getData().subscribe(
-      (listPlant: any[]) => {
-        console.log(listPlant);
+      /**
+       * Technique avec Underscore JS pour recupérer les catégories uniques de nos plantes
+       */
+      const listAllCategories = listPlant.map(
+        (product) => product.product_breadcrumb_label
+      );
+      console.log(listAllCategories);
 
-        /**
-         * Technique avec Underscore JS pour recupérer les catégories uniques de nos plantes
-         */
-        const listAllCategories = listPlant.map(product => product.product_breadcrumb_label);
-        console.log(listAllCategories);
-        
-        const listUniqCategories = _.uniq(listAllCategories) 
-        console.log(listUniqCategories);
-        
+      const listUniqCategories = _.uniq(listAllCategories);
+      console.log(listUniqCategories);
 
-        /**
-         * Technique native JS pour recupérer les catégories uniques de nos plantes
-         */
+      /**
+       * Technique native JS pour recupérer les catégories uniques de nos plantes
+       */
 
-        const listUniqJsCategories = [...new Set(listAllCategories)];
-        console.log(listUniqJsCategories);
+      const listUniqJsCategories = [...new Set(listAllCategories)];
+      console.log(listUniqJsCategories);
 
-        this.listCategoriesFilter = listUniqJsCategories;
-        this.listData = listPlant;
-        this.listData.length = 9;
-      }
-    )
+      this.listCategoriesFilter = listUniqJsCategories;
+      this.listData = listPlant;
+      this.listData.length = 9;
+    });
   }
 
   onEventLike() {
-    this.plantouneService.plantLiked$.next('')
+    this.plantouneService.plantLiked$.next('');
   }
 
+  onPriceFiltered(minmaxValues: MinMax) {
+    const minVal = minmaxValues.min;
+    const maxVal = minmaxValues.max;
+    console.log(`Filtrage des prix : ${minVal}, ${maxVal}`);
+    this.filterByPrice(minVal, maxVal);
+  }
+
+  private filterByPrice(minValue: number, maxValue: number) {
+    this.listDataFilteredByPrice = this.listData.filter((product) => {
+      return (
+        parseInt(product.product_unitprice_ati) <= maxValue &&
+        parseInt(product.product_unitprice_ati) >= minValue
+      );
+    });
+    console.log('Plantes filtrées par prix : ');
+    console.log(this.listDataFilteredByPrice);
+  }
 }
