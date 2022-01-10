8113149c09a2e8a6e15b23cb8bb3369b1c7e849d
William Noris <w.noris@protonmail.com>
2022-01-10 10:34:11 +0100

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { PageAccueilComponent } from './pages/page-accueil/page-accueil.component';
+import { PageDetailsComponent } from './pages/page-details/page-details.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { FilterSideBarComponent } from './components/filter-side-bar/filter-side-bar.component';
+import { CardPlanteComponent } from './components/card-plante/card-plante.component';
+import { HttpClientModule } from '@angular/common/http';
+import { IconComponent } from './components/icon/icon.component';
+import { AvisBarComponent } from './components/avis-bar/avis-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    PageAccueilComponent,
+    PageDetailsComponent,
+    PageNotFoundComponent,
+    FilterSideBarComponent,
+    CardPlanteComponent,
+    IconComponent,
+    AvisBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/pages/page-accueil/page-accueil.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { PlantouneService } from 'src/app/services/plantoune.service';
+import * as _ from 'underscore';
+
+@Component({
+  selector: 'app-page-accueil',
+  templateUrl: './page-accueil.component.html',
+  styleUrls: ['./page-accueil.component.scss']
+})
+export class PageAccueilComponent implements OnInit {
+  public listData: any[];
+  public listCategoriesFilter: string[];
+
+  constructor(private plantouneService: PlantouneService) {
+    this.listData = [];
+    this.listCategoriesFilter = [];
+   }
+
+   /**
+    * equivalent de la ligne du dessus 
+    * 
+    * plantouneService;
+    * 
+    * constructor(plantouneService: PlantouneService) {
+    *   this.plantouneService = plantouneService;
+    * }
+    */
+
+
+
+  ngOnInit(): void {
+
+    this.plantouneService.getData().subscribe(
+      (listPlant: any[]) => {
+        console.log(listPlant);
+
+        /**
+         * Technique avec Underscore JS pour recupérer les catégories uniques de nos plantes
+         */
+        const listAllCategories = listPlant.map(product => product.product_breadcrumb_label);
+        console.log(listAllCategories);
+        
+        const listUniqCategories = _.uniq(listAllCategories) 
+        console.log(listUniqCategories);
+        
+
+        /**
+         * Technique native JS pour recupérer les catégories uniques de nos plantes
+         */
+
+        const listUniqJsCategories = [...new Set(listAllCategories)];
+        console.log(listUniqJsCategories);
+
+        this.listCategoriesFilter = listUniqJsCategories;
+        this.listData = listPlant;
+        this.listData.length = 9;
+      }
+    )
+  }
+
+  onEventLike() {
+    this.plantouneService.plantLiked$.next('')
+  }
+
+}
